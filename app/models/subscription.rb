9d2d06cbf5233022ac8cbb215b73bfcb3d236557
Jack Woodrow <me@jackwoodrow.io>
2020-04-16 16:41:38 -0400

    5.1 - created subscriptions and relationship to publication

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,12 +1,10 @@
-class Publication < ApplicationRecord
+class Subscription < ApplicationRecord
   # Adds in the friendlyId into the model
   extend FriendlyId
   # Sets the default find finder to slugged but defaults if not found to default finders like ID
   friendly_id :uuid, use: %i[slugged finders]
 
-  has_many :articles
-  has_one :subscription
+  belongs_to :publication
 
   validates :name, presence: true
-  validates :description, presence: true
 end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :subscription do
+    name { "MyString" }
+    publication_id { 1 }
+  end
+end
